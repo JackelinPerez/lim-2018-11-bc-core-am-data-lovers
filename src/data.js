5019e0191fd0e21248c63ac74ea80310622662f5
AthalaAB <athala.abanto@gmail.com>
2018-12-12 10:01:49 -0500

    Uniendo códigos

--- src/data.js
+++ src/data.js
@@ -1,8 +1,91 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const filterDataFunction = (data, dataFilter, condition) => {
 
-const example = () => {
-  return 'example';
+  let arrayFilter = [];
+  let newArrayFilter = [];
+
+  switch (condition) {
+    case 0:
+      for (let i = 0; i < data.length; i++) {
+        arrayFilter.push(data[i].num);
+        if (arrayFilter[i].indexOf(dataFilter)!==-1)
+          newArrayFilter.push(data[i]);
+      }
+      break;
+    case 1:
+      for (let i = 0; i < data.length; i++) {
+        arrayFilter.push(data[i].name.toLowerCase());
+        if (arrayFilter[i].indexOf(dataFilter.toLowerCase())!==-1)
+          newArrayFilter.push(data[i]);
+      }
+      break;
+    case 2:
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].type.length; j++) {
+          arrayFilter.push(data[i].type[j].toLowerCase());
+          if (arrayFilter[j].indexOf(dataFilter.toLowerCase())!==-1)
+            newArrayFilter.push(data[i]);
+        }
+        arrayFilter = [];
+      }
+      break;
+    case 3:
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].weaknesses.length; j++) {
+          arrayFilter.push(data[i].weaknesses[j].toLowerCase());
+          if (arrayFilter[j].indexOf(dataFilter.toLowerCase())!==-1)
+            newArrayFilter.push(data[i]);
+        }
+        arrayFilter = [];
+      }
+      break;
+    default: newArrayFilter.push('No se eligio Tipo de filtro');
+  }
+  return newArrayFilter;
 };
 
-window.example = example;
+const sortDataFunction = (data, sortBy, sortOrder) =>{
+
+  let newArrayFilter = [];
+
+  for (let i = 0; i < data.length; i++)
+    newArrayFilter.push(data[i]);
+
+  if(sortBy === 0){
+    //Ordenas por ID
+    newArrayFilter.sort(
+      function(a,b){
+        if(sortOrder === 0) return a.id - b.id;
+        else return b.id - a.id;
+      }
+    );
+  }
+  else {
+    //Ordenar por A-Z
+    newArrayFilter.sort(
+      function (a,b){
+        if(sortOrder === 0){
+          if (a.name > b.name) return 1 ;
+          else return -1;
+        }
+        else {
+          if (a.name < b.name) return 1 ;
+          else return -1;
+        }
+      }
+    );
+  }
+  return newArrayFilter ;
+}
+
+// const computeStatsFunction = (data) =>{
+//   return 1;
+// }
+window.data = {
+  filterData: filterDataFunction,
+  sortData: sortDataFunction,
+  //computeStats: computeStatsFunction,
+}
+
+const prueba3 = filterDataFunction(POKEMON.pokemon,'char',1);
+console.log(prueba3);
+console.log(sortDataFunction(prueba3, 1, 0));
--- src/main.js
+++ src/main.js
@@ -1,20 +1,45 @@
-const vamosAPagSegunda= document.getElementsByClassName("pokedex");
-vamosAPagSegunda.addEventListener("click", function aPagPokedex(){
-element.classList.remove(pokedex);}
-
 
 /*const vamosAPagSegunda= document.getElementById("pokedex");
 vamosAPagSegunda.addEventListener("click", function pag2(){
    document.getElementById("inicio").style.display = "none";
    document.getElementById("pokedex").style.display = "block";
-})
-const vamosAPagTercera= document.getElementById("descarga");
-vamosAPagTercera.addEventListener("click", function pag3(){
-   document.getElementById("inicio").style.display = "none";
-   document.getElementById("descarga").style.display = "block";
-})
-const vamosAPag1= document.getElementById("inicio");
-vamosAPag1.addEventListener("click", function pag1(){
-   document.getElementById("inicio").style.display = "block";
-   document.getElementById("pokedex").style.display = "none";
 }) */
+
+// const vamosAPagInicio= document.getElementById("pp1");
+// vamosAPagInicio.addEventListener("click", function pag1(){
+//    document.getElementById("inicio").style.display = "block";
+//    const prInicio= document.getElementById("inicio");
+//    prInicio.classList.add("pokedexp");
+// })
+
+const vamosAPagPokedex= document.getElementById("pp2");
+vamosAPagPokedex.addEventListener("click", function pag2(){
+   document.getElementById("inicio").style.display = "none";
+   const prueba= document.getElementById("pokedex");
+   prueba.classList.remove("pokedexp");
+})
+
+ 
+
+
+
+const textfilter= document.getElementById('textfilter');
+const buttonSearch= document.getElementById('buttonSearch');
+const viewListFilter=document.getElementById('viewListFilter');
+
+const functionfilter =(data)=>{
+  let newGrill = [];
+  for (let i = 0; i < data.length; i++) {
+    newGrill.push('<div class="grid-item">'
+                  +'<input type="image" src="'+data[i].img +'" >'+'<br>'
+                  + '<label>'+data[i].num+'</label>'+'<br>'
+                  + '<label>'+data[i].name+'</label>'+'<br>'
+                  + '</div>');
+  }
+  viewListFilter.innerHTML= newGrill.join('');
+}
+
+buttonSearch.addEventListener('click',function(){
+  const prueba = data.filterData(POKEMON.pokemon,textfilter.value,1);
+  functionfilter(sortDataFunction(prueba, 1, 0));
+});
