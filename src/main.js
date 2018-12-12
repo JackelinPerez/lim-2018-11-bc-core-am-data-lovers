870c7ad0b337236c4e2da609c9c312f83baad24e
AthalaAB <athala.abanto@gmail.com>
2018-12-12 05:56:41 -0500

    Implementación de Grid

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-const vamosAPagSegunda= document.getElementById("inicio");
+const vamosAPagSegunda= document.getElementsByClassName("pokedex");
+vamosAPagSegunda.addEventListener("click", function aPagPokedex(){
+element.classList.remove(pokedex);}
+
+
+/*const vamosAPagSegunda= document.getElementById("pokedex");
 vamosAPagSegunda.addEventListener("click", function pag2(){
-   document.getElementById("primera-Pag").style.display = "none";
-   document.getElementById("segunda-Pag").style.display = "block";
+   document.getElementById("inicio").style.display = "none";
+   document.getElementById("pokedex").style.display = "block";
 })
-const vamosAPagTercera= document.getElementById("pokedex");
+const vamosAPagTercera= document.getElementById("descarga");
 vamosAPagTercera.addEventListener("click", function pag3(){
-   document.getElementById("primera-Pag").style.display = "none";
-   document.getElementById("tercera-Pag").style.display = "block";
+   document.getElementById("inicio").style.display = "none";
+   document.getElementById("descarga").style.display = "block";
 })
-const vamosAPag1= document.getElementById("descarga");
+const vamosAPag1= document.getElementById("inicio");
 vamosAPag1.addEventListener("click", function pag1(){
-   document.getElementById("primera-Pag").style.display = "block";
-   document.getElementById("segunda-Pag").style.display = "none";
-})
+   document.getElementById("inicio").style.display = "block";
+   document.getElementById("pokedex").style.display = "none";
+}) */
