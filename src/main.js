1e64fed07ceae70dc3f7bde68150066bff29f0e2
JackelinPerez <filomenochahua@gmail.com>
2018-12-13 15:36:09 -0500

    mostrar todas la lista al inicio, filtrar por tipo, nombre, ID, debilidades y ordenarlos por nombre o ID

--- src/main.js
+++ src/main.js
@@ -1,39 +1,23 @@
 const textfilter= document.getElementById('textfilter');
 const buttonSearch= document.getElementById('buttonSearch');
 const viewListFilter=document.getElementById('viewListFilter');
-// const vamosAPagInicio= document.getElementById("pp1");
-// vamosAPagInicio.addEventListener("click", function pag1(){
-//    document.getElementById("inicio").style.display = "block";
-//    const prInicio= document.getElementById("inicio");
-//    prInicio.classList.add("pokedexp");
-// })
+
 const prueba= document.getElementById("pokedex");
 const pagina2= document.getElementById("inicio");
 const vamosAPagPokedex= document.getElementById("pp2");
 
-let listenOption='';
-let arrayListenOption=[];
+
 vamosAPagPokedex.addEventListener("click", function pag2(){
    pagina2.style.display = "none";
    prueba.classList.remove("pokedexp");
 })
 
-const secondMenu= document.getElementById('second-menu');
-
-secondMenu.addEventListener("change",function(){
-  arrayListenOption=[];
-  listenOption=secondMenu.options[secondMenu.selectedIndex].value;
-  arrayListenOption[0]=parseInt(listenOption[0]);
-  arrayListenOption[1]=parseInt(listenOption[1]);
-
-  const prueba = filterDataFunction(POKEMON.pokemon,textfilter.value,1);
-  functionfilter(sortDataFunction(prueba,arrayListenOption[0], arrayListenOption[1]));
-});
-
-
+const orderAs = document.getElementById("orderAs");
+const filterAs = document.getElementById("filterAs");
 
 const functionfilter =(data)=>{
   let newGrill = [];
+  viewListFilter.value = '';
   for (let i = 0; i < data.length; i++) {
     newGrill.push('<div class="grid-item">'
                   +'<input type="image" src="'+data[i].img +'" >'+'<br>'
@@ -44,12 +28,20 @@ const functionfilter =(data)=>{
   viewListFilter.innerHTML= newGrill.join('');
 }
 
-buttonSearch.addEventListener('click',function(){
-  const prueba = filterDataFunction(POKEMON.pokemon,textfilter.value,1);
-  if(arrayListenOption.length===0){
-    listenOption=secondMenu.options[secondMenu.selectedIndex].value;
-    arrayListenOption[0]=parseInt(listenOption[0]);
-    arrayListenOption[1]=parseInt(listenOption[1]);
-  }
-  functionfilter(sortDataFunction(prueba,arrayListenOption[0], arrayListenOption[1]));
-});
+const functionListenFilterOrder = ()=>{
+  const listenOrderAs = orderAs.options[orderAs.selectedIndex].value;
+  const listenFilterAs = filterAs.options[filterAs.selectedIndex].value;
+  const arrayInputFilter = data.filterData(POKEMON.pokemon,textfilter.value,parseInt(listenFilterAs[0]));
+
+  functionfilter(data.sortData(arrayInputFilter,parseInt(listenOrderAs[0]), parseInt(listenOrderAs[1])));
+  return 1;
+}
+
+const functionMain = () =>{
+  functionListenFilterOrder();
+  filterAs.addEventListener("change",functionListenFilterOrder);
+  orderAs.addEventListener("change",functionListenFilterOrder);
+  buttonSearch.addEventListener('click',functionListenFilterOrder);
+}
+
+functionMain();
